546ec508d531280f5a366e8e8336f0abe81d4b16
Erick Grilo <simasgrilo86@gmail.com>
2018-03-31 18:19:56 -0300

    Primeiro teste do Parser

--- TerceiroTeste/Program.fs
+++ TerceiroTeste/Program.fs
@@ -7,24 +7,32 @@ open System
 
 
 //quem conseguir fazer isso funcionar ganha um premio. ele diz que uma gramática é umac oleção de regras mas não fala como que essa coleção tem que ser estruturada.
+
 type PEGParser () = 
-    member this.number = oneOf "0123456789" --> fun a -> int(a) - int('0')
-    member this.negNumber = ~~"-" + this.number --> fun a -> int (-snd(a))- int('0')
-    member this.addRule =  (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) + b
-    member this.subRule = (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) - b
-    member this.divRule = (this.number |- this.negNumber) + ~~"/" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) / b
-    member this.mulRule = (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) * b
+        //math rules
+        member this.number = oneOf "0123456789" --> fun a -> int(a) - int('0')
+        member this.negNumber = ~~"-" + this.number --> fun a -> int (-snd(a))- int('0')
+        //member this.arithop = oneOf "+-*/|"
+        //member this.exp = this.id
+        member this.addRule =  (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) + b
+        member this.subRule = (this.number |- this.negNumber) + ~~"-" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) - b
+        member this.divRule = (this.number |- this.negNumber) + ~~"/" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) / b
+        member this.mulRule = (this.number |- this.negNumber) + ~~"*" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) * b
 
-    //let PEGParser =
-     //   let number = oneOf "0123456789" --> fun a -> int(a) - int('0')
-     //   let negNumber = ~~"-" + this.number --> fun a -> int (-snd(a))- int('0')
-     //   let addRule =  (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) + b
-     //   let subRule = (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) - b
-     //   let divRule = (this.number |- this.negNumber) + ~~"/" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) / b
-     //   let mulRule = (this.number |- this.negNumber) + ~~"+" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) * b
+        //boolean expressions: me perdi
+        //member this.eqRule = (this.number |- this.negNumber) + ~~"==" + (this.number |- this.negNumber) --> fun (a,b) -> fst(a) = b
+       
 
-//[<EntryPoint>]
+       //commands:
+        //member this.assignRule =  + 
+   
+
+[<EntryPoint>]
 let main argv = 
+    if 3 = 3 then printfn "a"
+    let testGrammar = new PEGParser()
+    let teste = parse testGrammar.eqRule "3==2"
+    printfn "%A" teste
     let sheila = Console.ReadLine()
     printfn "%A" sheila
     //printfn "%A" argv
