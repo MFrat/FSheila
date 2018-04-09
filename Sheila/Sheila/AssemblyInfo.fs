214a91369c7f660f3152974c5f9749a6b5f14760
Vitor Lourenço <vitornaslourenco@gmail.com>
2018-04-09 09:26:59 -0300

    organizacao do rep - v.sheila

---
+++ Sheila/Sheila/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Sheila.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// As informações gerais sobre um assembly são controladas por meio do seguinte
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associados a um assembly.
+[<assembly: AssemblyTitle("Sheila")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Sheila")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Definir ComVisible como false oculta os tipos neste assembly
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[<assembly: ComVisible(false)>]
+
+// O GUID a seguir é para a ID de typelib quando este projeto é exposto a COM
+[<assembly: Guid("5cbac859-acd6-4852-bc7d-e51bb7e2a82c")>]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//       Versão Principal
+//       Versão Secundária
+//       Número da Versão
+//       Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números da Versão e da Revisão
+// utilizando o '*' como mostrado abaixo:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
