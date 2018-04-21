f4adfa078fe25c8bb972a2984823fbf01a61632a
Erick Grilo <simasgrilo86@gmail.com>
2018-04-21 15:12:19 -0300

    Estrutura rudimentar de memória da SMC adicionada.

--- src/FSheila/Smc.fs
+++ src/FSheila/Smc.fs
@@ -5,6 +5,7 @@ module FSheila.Smc
 open ScanRat
 open FSheila
 open Parser
+open System.Collections.Generic
 
 
 //Protótipo de SMC Machine:
@@ -46,13 +47,24 @@ let rec calcbool exp = //a ideia parece ser exatamente isso
 
 //structs costumam ser mais eficientes que classes quando com poucos membros
 //https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/structures
-type SMC = //parece ser melhpr transformar isso num tipo record
+type SMC = //parece ser melhor transformar isso num tipo record
     struct
        val S : Stack  
-       val M : Exp list  //criar uma função que mapeie uma var -> conteúdo na memória (um inteiro ou um booleano, de acordo com o que for definido para a variável.   //não gostei de Exp list, mas..
+       val M : Dictionary<string,Exp> //M é um dicionário onde a chave é uma string e valor é uma Exp(booleano ou numérico) //Exp list  //criar uma função que mapeie uma var -> conteúdo na memória (um inteiro ou um booleano, de acordo com o que for definido para a variável.   //não gostei de Exp list, mas..
        val C : Stack 
        //new() = {S = [], M = [], C = []} //isso eu acho que eu já ganho por padrão. A inicialização default da estrutura consiste nas três composições estando vazias a princípio.
     end
+
+//regras da SMC
+//manipulação da memória
+let getFromMemory (s:SMC) (var:string) = s.M.Item(var) //retorna o valor de M associado á variável var
+
+let addToMemory (s:SMC) (var:string) (e:Exp) = s.M.Add(var,e)
+
+//regra de introdução do comando Assign
+//let varIntro (smc:SMC) =
+//    match (pop(smc.C)) with
+//    | Assign (a,b) -> push(a smc.S)
 
 let getFromParser exp (smc:SMC) =
     match exp with
@@ -63,3 +75,6 @@ let math exp =
     match exp with
     | Success s -> calculator s.value
     | Failure f -> failwith "deu bosta então se foda"   
+
+
+    //de acordo com as transições em SMC (descritas nas páginas 15, 16 e 17
