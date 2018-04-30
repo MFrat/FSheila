0b450cfa704d7f4e9f9368a2d4baab76a5fc477f
Erick Grilo <simasgrilo86@gmail.com>
2018-04-29 22:01:53 -0300

    Smc the beginning

--- src/FSheila/Smc.fs
+++ src/FSheila/Smc.fs
@@ -7,84 +7,50 @@ open Parser
 open System.Collections.Generic
 
 let S = new Stack<Values>()
-let M = new Dictionary<string, Cmd>()
+let M = new Dictionary<string, Values>()
 let C = new Stack<Cmd>()
 
 
 //hello there.
-//até pensei em fazer uma calculadora separada, mas a parte de empilhar na pilha quebra.
-let rec aKindOfMagic (S: Stack<Values>) (M: Dictionary<string, Cmd>) (C: Stack<Cmd>) =
+//let calculatorTabajara exp1 exp2 =
+//    match exp1 with
+//    | Add (x,y) -> match x,y with
+//    | Subtract (x,y) 
+//    | Multiply (x,y)
+//    | Divide (x,y)
+//    | And (x,y)
+//    | Or of Exp * Exp
+//    | Neg of Exp
+//    | Eq of  Exp * Exp
+//    | Leb of Exp * Exp
+//    | Leq of Exp * Exp
+//    | Geb of Exp * Exp
+//    | Geq of Exp * Exp
+//    | Neq of Exp * Exp
+let rec aKindOfMagic (S: Stack<Values>) (M: Dictionary<string, Values>) (C: Stack<Cmd>) =
     if C.Count <> 0 then  
         printSMC S M C
         let op = C.Pop()
         match op with
             | Assign (a,b) -> S.Push(Values.Id(a)); C.Push(CmdAssign); match b with
+                              | Number x -> S.Push(Values.Number(x)); aKindOfMagic S M C
                               | Add (x,y) -> match x,y with
-                                            | Number a, Number b -> S.Push(Values.Number(b));S.Push(Values.Number a); C.Push(CmdAdd) ; aKindOfMagic S M C
+                                            | Number a, Number b -> S.Push(Values.Number(b));S.Push(Values.Number a); C.Push(CmdAdd) ; aKindOfMagic S M C 
                                             //| a ,b -> 
             //leia o Assign assim como foi feito no Plotkin: ele faz a regra C := I , empilhando o Id da variavel na pilha de valores, a operação de atribuição e a expressão a qual será atribuida                  
             | CmdAdd -> match S.Pop(), S.Pop() with
                         | Values.Number x, Values.Number y -> S.Push(Values.Number(x + y)); aKindOfMagic S M C
                         | a, b -> aKindOfMagic S M C //esse caso nunca vai cair.
-             
-            
-
-            | Sheila a -> match a with
-                            | "Add" -> aKindOfMagic S M C; C.Push(XSheila "Add"); aKindOfMagic S M C
-                            | "Subtract" -> aKindOfMagic S M C; C.Push(XSheila "Subtract"); aKindOfMagic S M C
-                            | "Multiply" -> aKindOfMagic S M C; C.Push(XSheila "Multiply"); aKindOfMagic S M C
-                            | "Divide" -> aKindOfMagic S M C; C.Push(XSheila "Divide"); aKindOfMagic S M C
-                            | "And" -> aKindOfMagic S M C; C.Push(XSheila "And"); aKindOfMagic S M C
-                            | "Or" -> aKindOfMagic S M C; C.Push(XSheila "Or"); aKindOfMagic S M C
-                            | "Neg" -> aKindOfMagic S M C; C.Push(XSheila "Neg"); aKindOfMagic S M C
-                            | "Eq" -> aKindOfMagic S M C; C.Push(XSheila "Eq"); aKindOfMagic S M C
-                            | "Neq" -> aKindOfMagic S M C; C.Push(XSheila "Neq"); aKindOfMagic S M C
-                            | "Leb" -> aKindOfMagic S M C; C.Push(XSheila "Leb"); aKindOfMagic S M C
-                            | "Leq" -> aKindOfMagic S M C; C.Push(XSheila "Leq"); aKindOfMagic S M C
-                            | "Geb" -> aKindOfMagic S M C; C.Push(XSheila "Geb"); aKindOfMagic S M C
-                            | "Geq" -> aKindOfMagic S M C; C.Push(XSheila "Geq"); aKindOfMagic S M C
-                            | "Assign" -> aKindOfMagic S M C; C.Push(XSheila "Assign"); aKindOfMagic S M C
-            | XSheila a -> match a with
-                            | "Add" -> match S.Pop(), S.Pop() with 
-                                        | Number x, Number y -> S.Push(Number (x + y))
-                                        | Id x, Number y -> match M.Item(x) with
-                                                            | Number c -> S.Push(Number(y + c))
-                            | "Subtract" -> match S.Pop(), S.Pop() with 
-                                        | Number x, Number y -> S.Push(Number (x - y))
-                            | "Multiply" -> match S.Pop(), S.Pop() with 
-                                        | Number x, Number y -> S.Push(Number (x * y))
-                            | "Divide" -> match S.Pop(), S.Pop() with 
-                                        | Number x, Number y -> S.Push(Number (x / y))
-                            | "And" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x && y))
-                            | "Or" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x || y))
-                            | "Neg" -> match S.Pop() with 
-                                        | Boolean x -> S.Push(Boolean(not(x)))
-                            | "Eq" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x = y))
-                                        | Number x, Number y -> S.Push(Boolean(x = y))
-                            | "Neq" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x <> y))
-                                        | Number x, Number y -> S.Push(Boolean(x <> y))
-                            | "Leb" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x < y))
-                            | "Leq" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x <= y))
-                            | "Geb" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x > y))
-                            | "Geq" -> match S.Pop(), S.Pop() with 
-                                        | Boolean x, Boolean y -> S.Push(Boolean(x >= y))
-                            | "Assign" -> match (S.Pop(), S.Pop()) with
-                                        | Id x, Number y -> try
-                                                             (M.Add(x,Number y))
+            //depois de ter resolvido tudo, o primeiro cara da pilha é um valor (numérico) e abaixo dele é o id:
+            | CmdAssign -> match S.Pop(), S.Pop() with
+                         | Values.Number y, Values.Id x -> try
+                                                             (M.Add(x,Values.Number y))
                                                             with
-                                                            | :? System.ArgumentException -> M.Remove(x); M.Add(x,Number y)
-                                        | Id x, Boolean y -> (M.Add(x,Boolean y))
-                                        | Id x, Id y -> (M.Add(x,M.Item(y)))
-            | Number x -> aKindOfMagic S M C; S.Push(Number x)
-            | Boolean x -> aKindOfMagic S M C; S.Push(Boolean x)
-            | Id x -> aKindOfMagic S M C; S.Push (Id x)
+                                                            | :? System.ArgumentException -> M.Remove(x); M.Add(x,Values.Number y)
+                         | Values.Boolean y, Values.Id x -> try
+                                                             (M.Add(x,Values.Boolean y))
+                                                            with
+                                                            | :? System.ArgumentException -> M.Remove(x); M.Add(x,Values.Boolean y)
 
     
     (*
